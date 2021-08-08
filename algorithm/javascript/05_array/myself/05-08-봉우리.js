dda90f2d04ffd0fc55568b2381a7709af575e43c
joker77z <joker77z@naver.com>
2021-08-08 10:53:19 +0900

    봉우리
    dx, dy를 선언해놓고 3중 for문을 시작한다.
    2중 for문은 행과 열을 순차적으로 도는 것이고
    마지막에 있는 for문은 4방향으로 탐색한다.
    탐색하는데 주위에 더 큰 숫자를 발견하면 지금 위치는 봉우리가 아니기 때문에
    break를 해서 다음 열에 있는 요소로 넘어간다.
    
    만약 주위에 전부(4방향 전부 다 탐색)탐색해도  지금위치보다 큰 것이 없다면 for else가 되게해서  flag=true를 그대로 받아 answer++된다.

---
+++ algorithm/javascript/05_array/myself/05-08-봉우리.js
@@ -0,0 +1,28 @@
+function solution(arr) {
+    let answer = 0;
+    let n = arr.length;
+    let dx = [-1, 0, 1, 0];
+    let dy = [0, 1, 0, -1];
+    for(let i=0; i<n; i++) {
+        for(let j=0; j<n; j++) {
+            let flag = true;
+            for(let k=0; k<4; k++) {
+                let nx = i + dx[k];
+                let ny = j + dy[k];
+                if(nx>=0 && nx<n && ny>=0 && ny<n && arr[nx][ny] > arr[i][j]) {// 주변 요소중에 더 큰게 있으면 지금 지점 끝낸다. 이건 봉우리가 아니다.
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) answer++;
+        }
+    }
+    return answer;
+}
+
+console.log(solution([[5, 3, 7, 2, 3], 
+    [3, 7, 1, 6, 1], 
+    [7, 2, 5, 3, 4], 
+    [4, 3, 6, 4, 1], 
+    [8, 7, 3, 5, 2]]
+    ))
