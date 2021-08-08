4e6f2d873d2f60fa88159475b5f45feaead8a989
joker77z <joker77z@naver.com>
2021-08-08 11:21:54 +0900

    옳바른 괄호
    괄호 '('가 나오면 stack에 push하고
    괄호 ')'가 나오면 stack.pop()을 하는 방법으로
    마지막에 stack.length>0이면 (가 더 많거나 )가 부족했었다는 것이니까
    answer에 NO가 나오게 한다.

--- algorithm/javascript/06_stack_deque_queue/myself/06-01-옳바른괄호.js
+++ algorithm/javascript/06_stack_deque_queue/myself/06-01-옳바른괄호.js
@@ -10,3 +10,22 @@
 
     마지막에서 만약 스택에 뭐가 남아있으면 return "NO"
 */
+
+function solution(data) {
+    let answer = "YES";
+    let stack = [];
+    for(let i=0; i<data.length; i++) {
+        if(data[i] === '(') {
+            stack.push(data[i]);
+        } else {
+            stack.pop(data[i]);
+        }
+    }
+    if(stack.length>0) {
+        answer = "NO";
+    }
+    return answer;
+}
+
+console.log(solution("(()(()))(()"));
+console.log(solution("(())"));
