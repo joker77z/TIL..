965af2dc1d387653c009a24126f426c1bad185a4
joker77z <joker77z@naver.com>
2021-08-10 09:55:04 +0900

    연속부분수열3
    이 문제도 동일한 방법으로 접근해야 한다.
    이 문제는 모든 부분수열을 구하는 것이기 때문에
    lt, rt를 이용해서 해당 위치에서 몇개나 추가되는지 파악하고
    공식으로 맞춰넣는다.
    
    point: answer += (rt-lt+1)로 계산하면 된다.

--- algorithm/javascript/07_sliding_window/myself/07-05연속부분수열3.js
+++ algorithm/javascript/07_sliding_window/myself/07-05연속부분수열3.js
@@ -17,3 +17,21 @@
     answer = answer + (rt-lt+1);
 */
 
+function solution(nums, m) {
+    let answer = 0;
+    let sum = 0;
+    let lt = 0;
+    
+
+    for(let rt=0; rt<nums.length; rt++) {
+        sum += nums[rt];
+        // if(sum<m) answer++;
+        while(sum>m) {
+            sum -= nums[lt++];
+        }
+        answer += (rt-lt+1);
+    }
+    return answer;
+}
+
+console.log(solution([1, 3, 1, 2, 3], 5));
