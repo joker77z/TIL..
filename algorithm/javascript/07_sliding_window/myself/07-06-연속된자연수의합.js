16f04a561a5656e4fb015e8bbed2f001ab440205
joker77z <joker77z@naver.com>
2021-08-10 09:56:41 +0900

    연속된 자연수의 합
    15까지 더하는데 2개 이상의 수로 더해야 하기 때문에 반으로 나누고 parseint하고 1더해서 1~8까지 있는 배열을 하나 만든다.
    
    그 다음에 다른 연속수열과 마찬가지로 m값에 달성하는 sum이면 answer++ 해주고 m을 넘어가는 sum이면 앞에서부터 하나씩 빼주면서 값을 달성했을 때 answer++해준다.

--- algorithm/javascript/07_sliding_window/myself/07-06-연속된자연수의합.js
+++ algorithm/javascript/07_sliding_window/myself/07-06-연속된자연수의합.js
@@ -12,3 +12,22 @@
     똑같이 lt, rt를 0부터 더해준다.
     sum이 m보다 넘어가면 다시 while(sum>m) sum -= arr[lt++]를 해주고 answer++해주면 된다.
 */
+
+function solution(m) {
+    let nums = Array.from({length:parseInt(m/2)+1}, (v, i) => i+1);
+    let lt = 0;
+    let sum = 0;
+    let answer = 0;
+
+    for(let rt=0; rt<nums.length; rt++) {
+        sum += nums[rt];
+        if(sum === m) answer ++;
+        while(sum>m) {
+            sum -= nums[lt++];
+            if(sum === m) answer++;
+        }
+    }
+    return answer;
+}
+
+console.log(solution(15));
