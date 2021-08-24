7caf319e07c32f8082e5ff0f8cea8f5dc6b09e92
joker77z <joker77z@naver.com>
2021-08-24 10:38:32 +0900

    등수구하기
    PseudoCode가 빛을 보고 있다.
    2중 for문을 사용해서 제일 첫번째 원소를 기준으로 시작 할 때 rank=1을 주고
    2번째 for문이 원소 첫번째~끝까지 돌면서 현재 i보다 더 큰 원소가 있으면 rank++해주면서
    2번째 for문이 끝났을 때 answer에 rank값을 더해준다.

---
+++ algorithm/1. 배열 탐색/등수구하기.js
@@ -0,0 +1,21 @@
+/*
+    등수를 출력.
+
+    같은 점수일 경우 높은 등수로 처리.
+    즉, 92점이 3명이면 1등이 3명, 그 다음은 4등.
+*/
+
+function solution(score) {
+    let answer = [];
+    for(let i=0; i<score.length; i++) {
+        let rank = 1;
+        for(let j=0; j<score.length; j++) {
+            if(score[i] < score[j]) rank++;
+        }
+        answer.push(rank);
+    }
+    return answer;
+}
+
+console.log(solution([87, 89, 92, 100, 76])) // 4 3 2 1 5
+console.log(solution([90, 90, 90, 82, 100])) // 4 3 2 1 5
