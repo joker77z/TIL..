741e0ca1d7e444976d6bf77275d3ee6bf295f6db
joker77z <joker77z@naver.com>
2021-08-26 23:02:31 +0900

    뒤집은 소수

---
+++ algorithm/3. 브루트 포스/뒤집은소수.js
@@ -0,0 +1,39 @@
+/*
+    자연수를 뒤집은 후 소수면 출력해라.
+    
+    Pseudo Code
+    for(x of nums)
+        let res=0;
+        let t=0;
+        while(x) {
+            t=x%10;
+            res=res*10+t;
+            x=x/10;
+        }
+*/
+// 소수인지 검증
+function isPrime(num) {
+    if(num===1) return false;
+    for(let i=2; i<=parseInt(Math.sqrt(num/2)); i++) {
+        if(num%i===0) return false;
+    }
+    return true;
+}
+
+function solution(nums) {
+    let answer = [];
+    for(x of nums) {
+        let res=0;
+        let t=0;
+        while(x) {
+            t=x%10;
+            res=res*10+t;
+            x=parseInt(x/10);
+        }
+        if(isPrime(res)) answer.push(res); 
+    }
+    return answer;
+}
+
+console.log(solution([32, 55, 62, 20, 250, 370, 200, 30, 100]))
+// 23 2 73 2 3
