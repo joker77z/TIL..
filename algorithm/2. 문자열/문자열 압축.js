3cd9706e0ddf08bf06c09178038daa45677941a2
joker77z <joker77z@naver.com>
2021-08-26 09:59:00 +0900

    base algorithm

---
+++ algorithm/2. 문자열/문자열 압축.js
@@ -0,0 +1,15 @@
+/*
+    문자가 연속으로 반복되는 경우
+    문자 뒤에 반복되는 숫자를 써준다.
+
+    Pseudo Code
+    ans = []
+    for 문자 하나씩 들어온다.
+        
+    
+*/
+function solution(s) {
+
+}
+
+console.log(solution("KKHSSSSSSSE"))
