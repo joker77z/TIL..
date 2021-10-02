e82716ef4e8281baf9febd7b0a29e74d847f714b
joker77z <joker77z@naver.com>
2021-10-02 20:44:02 +0900

    prettier 적용

--- javascript/NKLCB_Javascript/polyfill/forEach.js
+++ javascript/NKLCB_Javascript/polyfill/forEach.js
@@ -1,19 +1,18 @@
 // forEach 메소드를 가지고 있다면
-if(!Array.prototype.forEach2) {
-    Array.prototype.forEach2 = function(callback, thisArg) {
-
-        // callback이 함수가 아니라면 에러를 전달한다.
-        if(typeof callback !== 'function') {
-            throw new TypeError(`${callback} is not a function`);
-        }
-
-        // thisArg가 undefined라면 window를 가리키게 한다.
-        thisArg = thisArg || global;
-
-        for(var i = 0; i<this.length; i++) {
-            callback.call(thisArg, this[i], i, this);
-        }
+if (!Array.prototype.forEach2) {
+  Array.prototype.forEach2 = function (callback, thisArg) {
+    // callback이 함수가 아니라면 에러를 전달한다.
+    if (typeof callback !== 'function') {
+      throw new TypeError(`${callback} is not a function`);
     }
+
+    // thisArg가 undefined라면 window를 가리키게 한다.
+    thisArg = thisArg || global;
+
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
 }
 
 const fruits = ['apple', 'banana'];
--- javascript/NKLCB_Javascript/polyfill/map.js
+++ javascript/NKLCB_Javascript/polyfill/map.js
@@ -1,22 +1,21 @@
 // Map 메소드가 없다면
-if(!Array.prototype.map2) {
-    Array.prototype.map2 = function(callback, thisArg) {
-
-        // callback이 함수가 아니라면 에러를 전달한다.
-        if(typeof callback !== 'function') {
-            throw new TypeError (`${callback} is not a function`);
-        }
-
-        // thisArg가 undefined라면 window를 가리키게 한다.
-        thisArg = thisArg || global;
-
-        const arr = [];
-        for(var i=0; i<this.length; i++) {
-            arr.push(callback.call(thisArg, this[i], i, this));
-        }
-        return arr;
+if (!Array.prototype.map2) {
+  Array.prototype.map2 = function (callback, thisArg) {
+    // callback이 함수가 아니라면 에러를 전달한다.
+    if (typeof callback !== 'function') {
+      throw new TypeError(`${callback} is not a function`);
     }
+
+    // thisArg가 undefined라면 window를 가리키게 한다.
+    thisArg = thisArg || global;
+
+    const arr = [];
+    for (var i = 0; i < this.length; i++) {
+      arr.push(callback.call(thisArg, this[i], i, this));
+    }
+    return arr;
+  };
 }
 
 const fruits = ['apple', 'banana'];
-console.log(fruits.map2(v => v+'2')); // [ 'apple2', 'banana2' ]
+console.log(fruits.map2(v => v + '2')); // [ 'apple2', 'banana2' ]
